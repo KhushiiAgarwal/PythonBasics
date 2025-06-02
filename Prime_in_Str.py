bc4e7bcc663ae9a6b8a7dea8480033ae1c14e969
Khushi A <99123453+KhushiiAgarwal@users.noreply.github.com>
2025-06-02 14:01:54 +0530

    Check if sum of digits in alphanumeric str is prime

--- Prime_in_Str.py
+++ Prime_in_Str.py
@@ -1,1 +1,16 @@
 #Remove all non-digit char and check if sum is prime.
+def isprime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num/2)):
+        if(num%i == 0):
+            return False
+    return True
+
+str = input()
+sum = 0
+for i in str:
+    if i.isdigit():
+        sum = sum + int(i)
+res = isprime(sum)       
+print(res)
